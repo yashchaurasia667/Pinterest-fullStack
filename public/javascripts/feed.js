a1729948d6a86c244974363b866f433c78616f23
Yash <yashchaurasia630@gmail.com>
2023-12-25 09:15:32 +0530

    completed 2 modals and pin hover effect

--- public/javascripts/feed.js
+++ public/javascripts/feed.js
@@ -10,11 +10,30 @@ function handleSearchFocus(selector, borderColor) {
     });
 }
 
-function showModal(modal) {
-    if (modal.hasAttribute('open'))
-        modal.removeAttribute('open');
-    else
-        modal.setAttribute('open', '');
+function showModal(button, modal) {
+    // console.log(modal)
+    if (document.querySelector(modal).hasAttribute('open'))
+        document.querySelector(modal).removeAttribute('open');
+    else {
+        document.querySelector(modal).setAttribute('open', '');
+        gsap.from(modal, {
+            height: '90px',
+            duration: 0.1,
+        })
+    }
+
+    button.addEventListener('blur', () => {
+        document.querySelector(modal).removeAttribute('open')
+    })
 }
 
+function activePage() {
+    let pages = document.querySelectorAll('#nav-left .pages-navbar')
+    for (let i = 0; i < 3; i++) {
+        if (window.location.pathname == pages[i].getAttribute('href'))
+            pages[i].classList.add('active-page')
+    }
+}
+
+activePage();
 handleSearchFocus('#navbar-search', '#7fc1ff');
